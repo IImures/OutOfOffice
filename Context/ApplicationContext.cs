3ee5c59acf49eaba3b11b8c692db134b25c235a3
Vladyslav Kalynchenko <vlad.kalinchenko215@gmail.com>
2024-06-22 15:09:31 +0200

    Feature: Added Employee DbSet to ApplicationContext

--- Context/ApplicationContext.cs
+++ Context/ApplicationContext.cs
@@ -1,9 +1,17 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using OutOfOffice.Entities;
 
 namespace OutOfOffice.Context;
 
 public class ApplicationContext : DbContext
 {
+    public DbSet<Employee> Employees { get; set; }
+    public DbSet<Subdivision> Subdivisions { get; set; }
+    public DbSet<Position> Positions { get; set; }
+    public DbSet<Role> Roles { get; set; }
+    public DbSet<Authority> Authorities { get; set; }
+    public DbSet<EmployeeStatus> EmployeeStatuses { get; set; }
+    
     public ApplicationContext()
     {
     }
